1cb031f3ec0e3d59b6b5e747773355d430c7dc17
Logan <logan.murphy@temple.edu>
2014-04-02 23:20:36 -0400

    redesigned the preprocessor

--- include/preprocessor.cpp
+++ include/preprocessor.cpp
@@ -4,46 +4,114 @@
 
 using namespace std;
 
+typedef struct {
+	string type;
+	string name;
+} field;
+
+typedef struct {
+	string name;
+	vector<field> fields;
+} clazz;
+
+void freeit(field f) {
+	if(f.type.find("vector") != string::npos) {
+		cout << "\t\tfor(int i = 0; i < " << f.name << ".size(); i++) {\n";
+		cout << "\t\t\tdelete " << f.name << "[i];\n";
+		cout << "\t\t}\n";
+	} else if(f.type.find("serializable") != string::npos) {
+		cout << "\t\tdelete " << f.name << ";\n";
+	}
+}
+
+string defaultValue(string type) {
+	if(type.find("vector") != string::npos) {
+		return " = std::vector<serializable *>();";
+	} else if(type.find("serializable") != string::npos) {
+		return " = NULL;";
+	} else if(type.find("string") != string::npos) {
+		return " = \"\";";
+	} else if(type.find("int") != string::npos) {
+		return " = 0;";
+	}	
+	return ";";
+}
+
 int main(void) {
-	vector<string> classNames;
-	string line;
-	bool isSerialize = false;
-	string serialize;
-	string className;
+	vector<clazz *> clazzez;
+	clazz * c;
+	bool serialize = false;
 	while(!cin.eof()) {
+		string line;
 		getline(cin, line);
-		if(line.find("#end") == 0 && isSerialize) {
-			isSerialize = false;
-			std::cout << "\tvoid serialize(datastream ds) {\n";
-			std::cout << serialize;
-			serialize = "";
-			std::cout << "\t};\n";
-			std::cout << "\t" << className << "(int id) : serializable(\"" << className << "\", id) {};\n";
-			std::cout << "\t" << className << "() : serializable(\"" << className << "\") {};\n";
-			std::cout << "};\n";
-			std::cout << "serializable * get_" << className << "(int id) {\n";
-			std::cout << "\t" << className << " * s = new " << className << "(id);\n"; 
-			std::cout << "\ts->load();\n";
-			std::cout << "\treturn s;\n";
-			std::cout << "}\n";
-		} else if(isSerialize) {
-			cout << line << "\n";
-			serialize += "\t\tds &" + line.substr(line.find_last_of(" ")) + "\n";
-		} else if(line.find("#serialize") == 0) {
-			int space = line.find(" ");
-			className = line.substr(space + 1);
-			classNames.push_back(className);
-			isSerialize = true;
-			std::cout << "class " << className << " : public serializable {\n";
-			std::cout << "public:\n";
+		if(serialize) {
+			if(line.find("#end") == 0) {
+				serialize = false;
+			} else {
+				field f;
+				int name = line.find_last_of(" ");
+				f.name = line.substr(name + 1);
+				f.type = line.substr(0, name);
+				c->fields.push_back(f);
+			}
 		} else {
-			cout << line << "\n";
+			if(line.find("#serialize") == 0) {
+				clazzez.push_back(c = new clazz());
+				c->name = line.substr(line.find(" ") + 1);
+				serialize = true;
+			} else if(line.length()) {
+				cout << line << "\n";
+			}
 		}
 	}
+	for(int i = 0; i < clazzez.size(); i++) {
+		c = clazzez[i];
+		cout << "class " << c->name << " : public serializable {\n";
+		cout << "public:\n";
+		//fields
+		for(int j = 0; j < c->fields.size(); j++) {
+			field f = c->fields[j];
+			cout << f.type << " " << f.name << ";\n";
+		}
+		//constructor(int)
+		cout << "\t" << c->name << "(int id) : serializable(\"" << c->name << "\", id) {\n";
+		for(int j = 0; j < c->fields.size(); j++) {
+			field f = c->fields[j];
+			cout << "\t\t" << f.name << defaultValue(f.type) << "\n";
+		}
+		cout << "\t};\n";
+		//constructor()
+		cout << "\t" << c->name << "() : serializable(\"" << c->name << "\") {\n";
+		for(int j = 0; j < c->fields.size(); j++) {
+			field f = c->fields[j];
+			cout << "\t\t" << f.name << defaultValue(f.type) << "\n";
+		}
+		cout << "\t};\n";
+		//serialize
+		cout << "\tvoid serialize(datastream ds) {\n";
+		for(int j = 0; j < c->fields.size(); j++) {
+			field f = c->fields[j];
+			cout << "\t\tds & " << f.name << ";\n";
+		}
+		cout << "\t};\n";
+		//deconstructor
+		cout << "\t~" << c->name << "() {\n";
+		for(int j = 0; j < c->fields.size(); j++) {
+			field f = c->fields[j];
+			freeit(f);
+		}
+		cout << "\t};\n";
+		//getter
+		cout << "\tstatic serializable * get_" << c->name << "(int id) {\n";
+		cout << "\t\treturn new " << c->name << "(id);\n";
+		cout << "\t};\n";
+		cout << "};\n";
+	}	
+	
 	cout << "void datastream::register_constructors() {\n";
-	for(int i = 0; i < classNames.size(); i++) {
-		cout << "\tdatastream::register_constructor(get_" + classNames[i] + ", \"" + classNames[i] + "\");\n";
+	for(int i = 0; i < clazzez.size(); i++) {
+		c = clazzez[i];		
+		cout << "\tregister_constructor(" << c->name << "::get_" << c->name << ", \"" << c->name << "\");\n"; 
 	}
 	cout << "}";
-	return 0;
 }
